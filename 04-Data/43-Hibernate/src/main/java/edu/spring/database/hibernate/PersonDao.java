2f009cfd15f29674be453f61a5cf55b397860332
Matthias Bachmann <M.Bachmann@itbgmbh.ch>
2016-12-08 10:21:20 +0100

    correct folder structure

--- 04-Data/43-Hibernate/src/main/java/edu/spring/database/hibernate/PersonDao.java
+++
@@ -1,9 +0,0 @@
-package edu.spring.database.hibernate;
-
-import edu.spring.database.model.PersonEntity;
-
-public interface PersonDao {
-
-    public void save(PersonEntity... personEntity);
-    
-}
